4befab3b7c47615fabdb1935601310e2522b0d74
Aleksandr Tkachev <r91tap@gmail.com>
2024-02-05 17:45:02 +0300

    Home Work

---
+++ MyHomWork_l5/ClassLibrary1/Class1.cs
@@ -0,0 +1,129 @@
+﻿namespace HomeWork5Library
+{
+    public class HomeWork5
+    {
+        public static void MaxDigits(string ReceivedString)
+        {
+            string[] str = ReceivedString.Split(" ");
+            int MaxDigits = 0;
+            int Index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                int CounterMemory = 0;
+                for (int j = 0; j < str[i].Length; j++)
+                {
+                    if (char.IsNumber(str[i][j]))
+                    {
+                        CounterMemory++;
+                    }
+                    if (CounterMemory > MaxDigits)
+                    {
+                        MaxDigits = CounterMemory;
+                        Index = i;
+                    }
+                }
+            }
+            if (MaxDigits > 0)
+                Console.WriteLine("Больше всего цифр в слове " + str[Index]);
+            else
+            {
+                Console.WriteLine("в словах нет цифр");
+            }
+        }
+        public static void LengthWord(string ReceivedString)
+        {
+            string[] str = ReceivedString.Split(" ");
+            string LengthWord = " ";
+            int RepetitionCounter = 0;
+            int repetition = 0;
+            int a = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if
+                (str[i].Length > a)
+                {
+                    a = str[i].Length;
+                    LengthWord = str[i];
+                }
+            }
+            foreach (string i in str)
+            {
+                if (LengthWord.CompareTo(i) == 0)
+                {
+                    repetition++;
+                }
+                if (LengthWord.Length == i.Length)
+                {
+                    RepetitionCounter++;
+                }
+            }
+            Console.WriteLine($"Самое длинное слово {LengthWord} Слов такой длинны: {RepetitionCounter}\n Количество повторений этого слова: {repetition}");
+        }
+        public static void ReplacingNumbers(string ReceivedString)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                string s = Convert.ToString(i);
+                string s1 = Convert.ToString(i);
+                switch (i)
+                {
+                    case 0:
+                        {
+                            s1 = "Ноль";
+                            break;
+                        }
+                    case 1:
+                        {
+                            s1 = "Один";
+                            break;
+                        }
+                    case 2:
+                        {
+                            s1 = "Два";
+                            break;
+                        }
+                    case 3:
+                        {
+                            s1 = "Три";
+                            break;
+                        }
+                    case 4:
+                        {
+                            s1 = "Четыре";
+                            break;
+                        }
+                    case 5:
+                        {
+                            s1 = "Пять";
+                            break;
+                        }
+                    case 6:
+                        {
+                            s1 = "Шесть";
+                            break;
+                        }
+                    case 7:
+                        {
+                            s1 = "Семь";
+                            break;
+                        }
+                    case 8:
+                        {
+                            s1 = "Восемь";
+                            break;
+                        }
+                    case 9:
+                        {
+                            s1 = "Девять";
+                            break;
+                        }
+
+                }
+
+                ReceivedString = ReceivedString.Replace(s, s1);
+            }
+
+            Console.WriteLine(ReceivedString);
+        }
+    }
+}
---
+++ MyHomWork_l5/MyHomWork_l5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using HomeWork5Library;
+
+Console.WriteLine("введите любую строку");
+string ReceivedString  = Console.ReadLine();
+int a = 1;
+Console.WriteLine("Чтоб найти слова, содержащие максимальное количество цифр введите 1!\nЧтоб найти самое длинное слово и определить, сколько раз оно встретилось в тексте введите 2!\nЧтоб заменить цифры от 0 до 9 на слова «ноль», «один», …, «девять» введите 3!");
+try
+{
+    while (a != 0)
+    {  
+        int c = Convert.ToInt32(Console.ReadLine());
+        switch (c)
+        {
+            case 1:
+                {
+                    HomeWork5.MaxDigits(ReceivedString);
+                    break;
+                }
+            case 2:
+                {
+                    HomeWork5.LengthWord(ReceivedString);
+                    break;
+                }
+            case 3:
+                {
+                    HomeWork5.ReplacingNumbers(ReceivedString);
+                    break;
+                }
+        }
+        Console.WriteLine("Если желаете выйти введите 0.\nЧтоб продолжиь любую другую цифру.");
+        a = Convert.ToInt32(Console.ReadLine());
+        if (a != 0)
+        {
+            Console.WriteLine("выберите команду.");
+        }
+
+
+       
+    }
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода. Приложение будет закрыто");
+}
+
+
+
+
+
+
